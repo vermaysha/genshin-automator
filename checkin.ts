6a66f040b90e6926f990e98667bf305d48d5bcc7
Ashary Vermaysha <vermaysha@gmail.com>
2022-09-02 07:04:38 +0700

    fix error

--- checkin.ts
+++ checkin.ts
@@ -25,7 +25,8 @@ const checkin = async (cookies, i) => {
   const mainAccount = highest(accounts)
 
   db.all(`SELECT id FROM redeemed WHERE uid = '${mainAccount.game_uid}', date = '${today()}'`, async (err, rows) => {
-    if (rows.length < 1) {
+    console.log('Traveller, you\'ve already checked in today')
+    if (rows?.length < 1) {
       return false
     }
 
