d5b8198ede92005654179d7a1069970fe383b7f0
Ashary Vermaysha <asharyver13@gmail.com>
2022-10-21 18:07:46 +0700

    Add Discord WebHook

---
+++ src/Types/Embed/Image/index.d.ts
@@ -0,0 +1,29 @@
+import Attachment from '../../Attachment'
+
+/**
+ * Embed Image Structure
+ *
+ * @link https://discordapp.com/developers/docs/resources/channel#embed-object-embed-image-structure
+ */
+export default interface EmbedImage {
+  /**
+   * Source URL of the image.
+   * (Only supports http(s) and attachments)
+   */
+  url: string | Attachment
+
+  /**
+   * A proxied URL of the thumbnail.
+   */
+  proxy_url?: string
+
+  /**
+   * Height of the thumbnail.
+   */
+  height?: number
+
+  /**
+   * Width of the thumbnail.
+   */
+  width?: number
+}
