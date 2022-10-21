7dbba6e52d25a529223af2fa56720e87f2be8ea5
Ashary Vermaysha <asharyver13@gmail.com>
2022-10-21 16:26:27 +0700

    Create build script

---
+++ build.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+
+const { build } = require('esbuild')
+const glob = require('glob')
+const entryPoints = glob.sync('./src/**/*.ts')
+const fs = require('fs')
+const chalk = require('chalk')
+
+fs.rmSync('build', { recursive: true, force: true })
+
+build({
+  entryPoints,
+  outbase: './src',
+  outdir: './build',
+  platform: 'node',
+  watch: false,
+  sourcemap: true,
+  minify: true,
+  target: 'es6',
+}).then(() => {
+  const fileList = ['package.json', 'package-lock.json', '.env']
+
+  for (const file of fileList) {
+    if (fs.existsSync(file)) {
+      fs.copyFileSync(file, `build/${file}`)
+    }
+  }
+
+  console.log(chalk.green('Build Success'))
+})
