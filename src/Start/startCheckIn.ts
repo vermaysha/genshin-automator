9f4d343be74e697660f5925c5d7c460164723b84
Ashary Vermaysha <asharyver13@gmail.com>
2022-10-22 10:08:06 +0700

    Change USER_AGENT

--- src/Start/startCheckIn.ts
+++ src/Start/startCheckIn.ts
@@ -6,7 +6,8 @@ import { Embed, Webhook } from '../Types'
 export async function startCheckIn(cookie: string) {
   const genshin = new Genshin(
     cookie,
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+    process.env.USER_AGENT ??
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
   )
 
   try {
--- src/Start/startRedeem.ts
+++ src/Start/startRedeem.ts
@@ -6,7 +6,8 @@ import DiscordWebhook from '../Lib/DiscordWebhook'
 export async function startRedeem(cookie: string) {
   const genshin = new Genshin(
     cookie,
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+    process.env.USER_AGENT ??
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
   )
 
   try {
